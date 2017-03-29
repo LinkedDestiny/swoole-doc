2168d4e3fb7455bd64b3a64c632c3bf5a624e4cb
LinkedDestiny <simonarthur2012@gmail.com>
2017-03-29 10:36:47 +0800

    fix

--- src/course/process/base_process.php
+++ src/course/process/base_process.php
@@ -8,39 +8,26 @@
 
 class BaseProcess
 {
-
-    private $process;
+    private $process;                   // process对象
 
     public function __construct()
     {
         $this->process = new swoole_process(array($this, 'run') , false , true);
-        //$this->process->daemon(true,true);
         $this->process->start();
-        
-        swoole_event_add($this->process->pipe, function ($pipe){
-            $data = $this->process->read();
-            echo "RECV: " . $data.PHP_EOL;
-        });
     }
 
     public function run($worker)
     {
-        swoole_timer_tick(1000, function($timer_id ) {
-            static $index = 0;
-            $index = $index + 1;
-            $this->process->write("Hello");
-            var_dump($index);
-            if( $index == 10 )
-            {
-                swoole_timer_clear($timer_id);
-            }
-        });
+        $worker->name("swoole_process");   // 设置进程名
+        for($i = 0; $i < 10; $i ++)
+        {
+            echo "process: {$i}\n";
+        }
     }
 }
 
 new BaseProcess();
 swoole_process::signal(SIGCHLD, function($sig) {
-  //必须为false，非阻塞模式
   while($ret =  swoole_process::wait(false)) {
       echo "PID={$ret['pid']}\n";
   }
